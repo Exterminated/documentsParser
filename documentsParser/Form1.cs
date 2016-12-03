de60bbcdd737cf7f1c342aaf96fbbf4d0713e771
Девяткин Федор <devjatkin.fjodr@gmail.com>
2016-12-03 17:54:39 +0300

    Ver 0.1
    Добавлено открытие doc и docx.
    Преобразование в байты

--- documentsParser/Form1.cs
+++ documentsParser/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using documentsParser;
 using System.Windows.Forms;
+using System.IO;
 
 namespace documentsParser
 {
@@ -10,21 +11,43 @@ namespace documentsParser
         {
             InitializeComponent();
         }
-
+        byte[] myDoc;
+        Stream myStream = null;
         private void open_button_Click(object sender, EventArgs e)
         {
             // Displays an OpenFileDialog so the user can select a Cursor.
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            openFileDialog1.Filter = "Документы|*.doc";
+            openFileDialog1.Filter = "Word old (*.doc)|*.doc|Word new (*.docx)|*.docx";
             openFileDialog1.Title = "Выберите документ";
 
-            // Show the Dialog.
-            // If the user clicked OK in the dialog and
-            // a .CUR file was selected, open it.
-            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            myStream = openFileDialog1.OpenFile();
+                            logBox.Text = "Открыт документ\n";
+                            myDoc = convertToByte(myStream);
+                            logBox.Text = "Преобразованно в байты\n";
+                            for (int i = 0; i < myDoc.Length; i++) logBox.Text += myDoc[i].ToString() + " ";
+                            
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
             }
         }
+        private byte[] convertToByte(Stream input) {
+            byte[] output = new byte[input.Length];
+            input.Read(output, 0, (int)input.Length);
+            return output;
+
+        }      
     }
 }
