fdee8b760e3d9d2b8f7c7ca42a3bb27a1d4bd271
Девяткин Федор <devjatkin.fjodr@gmail.com>
2016-12-04 13:02:40 +0300

    version 0.1.1
    Начата разработка модуля замены

--- documentsParser/Form1.Designer.cs
+++ documentsParser/Form1.Designer.cs
@@ -30,11 +30,13 @@
         {
             this.open_button = new System.Windows.Forms.Button();
             this.logBox = new System.Windows.Forms.RichTextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.replace_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // open_button
             // 
-            this.open_button.Location = new System.Drawing.Point(386, 13);
+            this.open_button.Location = new System.Drawing.Point(382, 13);
             this.open_button.Name = "open_button";
             this.open_button.Size = new System.Drawing.Size(133, 23);
             this.open_button.TabIndex = 0;
@@ -44,17 +46,37 @@
             // 
             // logBox
             // 
-            this.logBox.Location = new System.Drawing.Point(13, 13);
+            this.logBox.Location = new System.Drawing.Point(13, 42);
             this.logBox.Name = "logBox";
-            this.logBox.Size = new System.Drawing.Size(317, 428);
+            this.logBox.Size = new System.Drawing.Size(317, 399);
             this.logBox.TabIndex = 1;
             this.logBox.Text = "";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(13, 13);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(317, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // replace_button
+            // 
+            this.replace_button.Enabled = false;
+            this.replace_button.Location = new System.Drawing.Point(382, 43);
+            this.replace_button.Name = "replace_button";
+            this.replace_button.Size = new System.Drawing.Size(133, 23);
+            this.replace_button.TabIndex = 3;
+            this.replace_button.Text = "Заменить";
+            this.replace_button.UseVisualStyleBackColor = true;
+            this.replace_button.Click += new System.EventHandler(this.replace_button_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(569, 453);
+            this.Controls.Add(this.replace_button);
+            this.Controls.Add(this.progressBar);
             this.Controls.Add(this.logBox);
             this.Controls.Add(this.open_button);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -70,6 +92,8 @@
 
         private System.Windows.Forms.Button open_button;
         private System.Windows.Forms.RichTextBox logBox;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button replace_button;
     }
 }
 
--- documentsParser/Form1.cs
+++ documentsParser/Form1.cs
@@ -2,6 +2,7 @@
 using documentsParser;
 using System.Windows.Forms;
 using System.IO;
+using System.Collections.Generic;
 
 namespace documentsParser
 {
@@ -13,11 +14,13 @@ namespace documentsParser
         }
         byte[] myDoc;
         Stream myStream = null;
+        List<string> replasment = new List<string>() { "строчка", "Строчка", "СТРОЧКА" };
+
         private void open_button_Click(object sender, EventArgs e)
         {
             // Displays an OpenFileDialog so the user can select a Cursor.
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            openFileDialog1.Filter = "Word old (*.doc)|*.doc|Word new (*.docx)|*.docx";
+            openFileDialog1.Filter = "Word 1997-2003 (*.doc)|*.doc|Word (*.docx)|*.docx";
             openFileDialog1.Title = "Выберите документ";
 
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
@@ -30,24 +33,40 @@ namespace documentsParser
                         {
                             myStream = openFileDialog1.OpenFile();
                             logBox.Text = "Открыт документ\n";
-                            myDoc = convertToByte(myStream);
-                            logBox.Text = "Преобразованно в байты\n";
-                            for (int i = 0; i < myDoc.Length; i++) logBox.Text += myDoc[i].ToString() + " ";
-                            
+                            myDoc = convertStreamToByte(myStream);                             
                         }
+                        replace_button.Enabled = true;
                     }
                 }
                 catch (Exception ex)
                 {
                     MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                    replace_button.Enabled = false;
                 }
             }
         }
-        private byte[] convertToByte(Stream input) {
+        private byte[] convertStreamToByte(Stream input) {
             byte[] output = new byte[input.Length];
             input.Read(output, 0, (int)input.Length);
+            logBox.Text = "Преобразованно в байты\n";
             return output;
+        }
+        
+        public void setProgressBar(int min, int max) {
+            progressBar.Minimum = min;
+            progressBar.Maximum = max;
+        }
 
-        }      
+        private void replace_button_Click(object sender, EventArgs e)
+        {
+            
+        }
+        public void replaceInDOCX(byte[] fileBytes, List<string> stringsToReplase) {
+            List<byte> bytes_stringToReplase=new List<byte>();
+            foreach (string s in stringsToReplase) bytes_stringToReplase.Add(Convert.ToByte(s));
+            for (int i = 0; i < fileBytes.Length; i++) {
+                if (bytes_stringToReplase.Contains(fileBytes[i])) ;//здесь какое-то действие чтобы заменить байты
+            }
+        }
     }
 }
